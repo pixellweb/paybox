c5650ed28448b44bbe9157cd46dd0ca6f0ad1237
B1nj <postmaster@b1nj.fr>
2025-10-24 12:46:10 -0400

    fix mode test ipn local

--- src/app/PaymentRequest.php
+++ src/app/PaymentRequest.php
@@ -53,7 +53,7 @@ class PaymentRequest
             'PBX_HASH'          => $hash_algo,
             'PBX_SIGN_KEYSIZE'  => 2048,
             'PBX_ANNULE'        => route(config('paybox.url_refuse')),
-            'PBX_EFFECTUE'      => route((!config('paybox.test_ipn_local') or !config('app.debug')) ? config('paybox.url_effectue') : config('paybox.url_repondre_a')),
+            'PBX_EFFECTUE'      => route((config('paybox.test_ipn_local') and config('app.debug')) ? config('paybox.url_repondre_a') : config('paybox.url_effectue')),
             'PBX_ATTENTE'       => route(config('paybox.url_attente')),
             'PBX_REFUSE'        => route(config('paybox.url_refuse')),
             'PBX_REPONDRE_A'    => route(config('paybox.url_repondre_a')),
